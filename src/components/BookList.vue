056dae29e22f0e3fb15c93b9041bc17b842df650
Steven Malikowski <cyclecoder@outlook.com>
2019-09-10 17:54:06 -0500

    Code would not verify, fixed an extra ;

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -62,7 +62,7 @@ export default {
   },
   computed: {
     filteredBooks() {
-      return _.filter(this.books, ["ownership", this.holding]);
+      return _.filter(this.books, ["ownership", this.holding])
     }
   },
   methods: {
