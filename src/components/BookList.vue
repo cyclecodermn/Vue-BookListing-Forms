d55afce1d3c76da19d7f98bc6e750fafab76464a
Steven Malikowski <cyclecoder@outlook.com>
2019-09-10 16:59:08 -0500

    Coded from video2, Fixed problem with finishedReading

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -20,9 +20,9 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finsihedReading: true },
-        { title: "American Gods", author: "Neil Gaiman", finsihedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finsihedReading: true }
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true },
+        { title: "American Gods", author: "Neil Gaiman", finishedReading: false },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true }
       ]
     };
   },
@@ -32,7 +32,7 @@ export default {
   },
   methods: {
     appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finsihedReading: bookData.finsihedReading });
+      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finsihedReading: bookData.finishedReading });
     }
   }
 };
